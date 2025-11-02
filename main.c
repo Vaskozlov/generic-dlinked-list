2b05f679991d95dbbb58a7407c6199a4d81c9987
vaskozlov <vaskozlov3940@gmail.com>
2025-11-02 10:27:18 +0300

    op/sec statistics

--- main.c
+++ main.c
@@ -6,25 +6,30 @@
 #include <stdatomic.h>
 #include <stdio.h>
 
+#define READING_THREADS 4
+#define WRITING_THREADS 4
+
 struct rwlock lock;
 atomic_uint writers_count;
 atomic_uint readers_count;
 atomic_uint reading_iterations;
 atomic_uint writing_iterations;
 
+struct timespec start;
+
 void *reader(void *)
 {
     while (true) {
         rwlock_rdlock(&lock);
 
-        atomic_fetch_add(&readers_count, 1);
+        atomic_fetch_add_explicit(&readers_count, 1, memory_order_relaxed);
 
-        if (atomic_load(&writers_count) > 0) {
+        if (atomic_load_explicit(&writers_count, memory_order_relaxed) > 0) {
             printf("Writing in progress!\n");
         }
 
-        atomic_fetch_sub(&readers_count, 1);
-        atomic_fetch_add(&reading_iterations, 1);
+        atomic_fetch_sub_explicit(&readers_count, 1, memory_order_relaxed);
+        atomic_fetch_add_explicit(&reading_iterations, 1, memory_order_relaxed);
 
         rwlock_rdunlock(&lock);
     }
@@ -37,16 +42,16 @@ void *writer(void *)
     while (true) {
         rwlock_wrlock(&lock);
 
-        if (atomic_fetch_add(&writers_count, 1) > 0) {
+        if (atomic_fetch_add_explicit(&writers_count, 1, memory_order_relaxed) > 0) {
             printf("More than one writers\n");
         }
 
-        if (atomic_load(&readers_count) > 0) {
+        if (atomic_load_explicit(&readers_count, memory_order_relaxed) > 0) {
             printf("Reading in progress!\n");
         }
 
-        atomic_fetch_sub(&writers_count, 1);
-        atomic_fetch_add(&writing_iterations, 1);
+        atomic_fetch_sub_explicit(&writers_count, 1, memory_order_relaxed);
+        atomic_fetch_add_explicit(&writing_iterations, 1, memory_order_relaxed);
 
         rwlock_wrunlock(&lock);
     }
@@ -60,10 +65,25 @@ void signal_handler(int sig)
     using namespace std;
 #endif
 
+    struct timespec end;
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    double elapsed_ms =
+        (double)((end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000LL)
+        / 1000.0;
+
+    const unsigned reads = atomic_load(&reading_iterations);
+    const unsigned writes = atomic_load(&writing_iterations);
+
     switch (sig) {
     case SIGINT:
         printf(
-            "r: %u, w: %u\n", atomic_load(&reading_iterations), atomic_load(&writing_iterations));
+            "r: %u, w: %u, r: %.0f op/sec, w: %.0f op/sec\n",
+            reads,
+            writes,
+            (double)reads / elapsed_ms,
+            (double)writes / elapsed_ms);
         break;
     default:
         exit(0);
@@ -94,15 +114,16 @@ int main()
     signal(SIGINT, signal_handler);
 
     rwlock_init(&lock);
-    pthread_t readers[4];
+    pthread_t readers[READING_THREADS];
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
-    for (size_t i = 0; i < 4; ++i) {
+    for (size_t i = 0; i < READING_THREADS; ++i) {
         pthread_create(&readers[i], NULL, reader, NULL);
     }
 
-    pthread_t writers[4];
+    pthread_t writers[WRITING_THREADS];
 
-    for (size_t i = 0; i < 4; ++i) {
+    for (size_t i = 0; i < WRITING_THREADS; ++i) {
         pthread_create(&writers[i], NULL, writer, NULL);
     }
 
